987fd1a30d931f6840dcc9412b42d796553c6bad
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-01 14:04:49 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using the urllib package
+"""Fetches the URL: https://intranet.hbtn.io/status
 """
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    data = response.read()
-    print("Body response:")
-    print("	- type:", type(data))
-    print("	- content:", data)
-    print("	- utf8 content:", data.decode('utf-8'))
+
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    req = Request('https://intranet.hbtn.io/status')
+
+    with urlopen(req) as res:
+        content = res.read()
+        utf8_content = content.decode('utf-8')
+
+        print('Body response:')
+        print('\t- type: {_type}'.format(_type=type(content)))
+        print('\t- content: {_content}'.format(_content=content))
+        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
